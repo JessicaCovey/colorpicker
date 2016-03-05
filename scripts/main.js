3bcea333cb9629202e2af29a459e36f96f8c40ec
Jessica Covey <coveyjessica@gmail.com>
2016-03-04 17:50:32 -0800

    use map() update

--- scripts/main.js
+++ scripts/main.js
@@ -11,64 +11,34 @@ var ColorPicker = React.createClass({
   },
 
   render : function() {
+    const {red, green, blue} = this.state;
     return (
       <div>
         <div
           style={{
-            width : 1200,
-            height : 500,
-            margin : 2,
-            border : '2px solid black',
-            backgroundColor : `rgb(${this.state.red}, ${this.state.green}, ${this.state.blue})`
+            width : 900,
+            height : 600,
+            backgroundColor : `rgb(${red}, ${green}, ${blue})`
           }}
         />
-        <div>
-          <input
-            type='range'
-            value={this.state.red}
-            onChange={
-              (e) => this.setState({red: e.target.value})
-            }
-            min={0}
-            max={255}
-          />
-          RED
-        </div>
-        <div>
-          <input
-            type='range'
-            value={this.state.green}
-            onChange={
-              (e) => this.setState({green: e.target.value})
-            }
-            min={0}
-            max={255}
-          />
-          GREEN
-        </div>
-        <div>
-          <input
-            type='range'
-            value={this.state.blue}
-            onChange={
-              (e) => this.setState({blue: e.target.value})
-            }
-            min={0}
-            max={255}
-          />
-          blue
-        </div>
+        {
+          ['red', 'green', 'blue'].map((color) =>
+            <div>
+              <input
+                type='range'
+                value={this.state[color]}
+                onChange={
+                  (e) => this.setState({[color]: e.target.value})
+                }
+                min={0}
+                max={255}
+              />
+              {color}
+            </div>
+          )
+        }
       </div>
     );
   }
 });
 ReactDOM.render(<ColorPicker/>, document.querySelector('#main'));
-{/*
-    1.  How do you style MARGIN with different values 2px, 40px?
-    2.  Same with BORDER sold 2px black?
-    3. Finally fully understood syntax, punctuation for render{}; return;
-    4. Repeated this fully several times including the React.createClass
-    5. fat function more comfy with. doing over and over jogged my todo list mem
-    6. this.setState made many mistakes there but get it now ex. this.state.red...this.setState{()}
-    7. better with self closing tags, input / div style
-*/}
